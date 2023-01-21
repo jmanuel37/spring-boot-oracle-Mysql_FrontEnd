6d6636f71870e809bace8db34533102795f73dc6
jmanuel37 <jmanuel.abarca@yahoo.com>
2023-01-21 00:23:11 -0600

    commit6

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 // const baseUrl = 'http://localhost:8887/api/tutorials';
- const baseUrl = '//3.87.198.91:80/api/tutorials';
+ const baseUrl = 'http://3.87.198.91:80/api/tutorials';
 //const baseUrl='https://reglibrobackend-aws.d30k0sly22hsyg.amplifyapp.com/api/tutorials';
 
 @Injectable({
